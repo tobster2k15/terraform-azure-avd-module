25b91931b9721f5d8b11df2a405093f9f5863600
tbraunzech <tobias.braun@wf-ib.de>
2024-03-06 06:45:44 +0100

    st access

--- resources_shd.tf
+++ resources_shd.tf
@@ -219,7 +219,7 @@ resource "azurerm_storage_share" "FSShare" {
 
 resource "azurerm_role_assignment" "af_role" {
   for_each           = toset(local.st_access)
-  scope              = azurerm_storage_account.storage.id
+  scope              = azurerm_storage_account.storage[count.index].id
   role_definition_id = data.azurerm_role_definition.storage_role.id
   principal_id       = each.key
 }
