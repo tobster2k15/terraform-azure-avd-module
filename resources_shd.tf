67a8edc2b00b18b175e7b735d1a3c5ff25295e04
tbraunzech <tobias.braun@wf-ib.de>
2024-04-03 13:11:26 +0200

    added element

--- resources_shd.tf
+++ resources_shd.tf
@@ -389,8 +389,8 @@ resource "azurerm_mysql_flexible_server" "mysql" {
 resource "azurerm_mysql_flexible_database" "mysqldb_prd" {
   count               = var.sql_enabled == true ? var.db_count : 0
   name                = "${local.sql_db_prd}-${format("%03d", count.index + 1)}"
-  resource_group_name = element(azurerm_resource_group.myrg_shd[count.index].name)
-  server_name         = element(azurerm_mysql_flexible_server.mysql[count.index].name)
+  resource_group_name = element(azurerm_resource_group.myrg_shd[*].name, count.index)
+  server_name         = element(azurerm_mysql_flexible_server.mysql[*].name, count.index)
   charset             = var.sql_charset
   collation           = var.sql_collation
 }
