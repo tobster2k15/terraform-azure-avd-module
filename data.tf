b3706250979784763c6a8d1ec889a4017c8270c0
tbraunzech <tobias.braun@wf-ib.de>
2024-04-05 10:23:50 +0200

    changed standard route

--- data.tf
+++ data.tf
@@ -27,7 +27,7 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
 data "template_file" "st_join" {
-    template = "${file("${path.root}/st_join.ps1")}"
+    template = "${file("st_join.ps1")}"
     vars = {
         
         ClientId                = "${var.ARM_CLIENT_ID}"
