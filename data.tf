b95c10e8b6e3f823007fb1e6fa283433d17d0bed
tbraunzech <tobias.braun@wf-ib.de>
2024-04-08 13:14:39 +0200

    stjoin removed

--- data.tf
+++ data.tf
@@ -26,19 +26,19 @@ data "azurerm_client_config" "current" {}
 
 data "azurerm_subscription" "current" {}
 
-data "template_file" "st_join" {
-    template = "${file("${path.root}/scripts/st_join.ps1")}"
-    vars = {
+# data "template_file" "st_join" {
+#     template = "${file("${path.root}/scripts/st_join.ps1")}"
+#     vars = {
         
-        ClientId                = "${var.ARM_CLIENT_ID}"
-        SubscriptionId          = "${var.ARM_SUBSCRIPTION_ID}"
-        ResourceGroupName       = "${local.rg_name_shd}"
-        StorageAccountName      = "${local.st_name}"
-        SamAccountName          = "${local.st_name}"
-        DomainAccountType       = "ComputerAccount"
-        IdentityServiceProvider = "ADDS"
-        OUName                  = "${var.st_ou_path}"
-        EncryptionType          = "AES256"
-        StorageAccountFqdn      = "${local.st_name}.file.core.windows.net"
-  }
-}
+#         ClientId                = "${var.ARM_CLIENT_ID}"
+#         SubscriptionId          = "${var.ARM_SUBSCRIPTION_ID}"
+#         ResourceGroupName       = "${local.rg_name_shd}"
+#         StorageAccountName      = "${local.st_name}"
+#         SamAccountName          = "${local.st_name}"
+#         DomainAccountType       = "ComputerAccount"
+#         IdentityServiceProvider = "ADDS"
+#         OUName                  = "${var.st_ou_path}"
+#         EncryptionType          = "AES256"
+#         StorageAccountFqdn      = "${local.st_name}.file.core.windows.net"
+#   }
+# }
